f05a609c0819ce9d67bc181eef152e3e4bd4e8da
Tyutyunnik Vladimir <kotoffwhite@gmail.com>
2022-11-25 22:15:12 +0300

    added accounts

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Django admin - администрация БД
     path('admin/', admin.site.urls),
+
+    # User managment
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    # Local apps
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('pages.urls', namespace='pages')),
 ]
